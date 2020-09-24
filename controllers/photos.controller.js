f939a0a2a5110948342abdb192e3b4a43bebe40d
Adrian Kryzinski <adriankryzinski@gmail.com>
2020-09-24 19:23:55 +0200

    [UPDATE] work on photos votes + add validation author & title length inmage file type

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -1,5 +1,5 @@
 const Photo = require('../models/photo.model');
-const Vote = require('../models/Voter.model');
+const Voter = require('../models/Voter.model');
 const requestIp = require('request-ip');
 
 /****** SUBMIT PHOTO ********/
@@ -13,19 +13,16 @@ exports.add = async (req, res) => {
     if(title && author && email && file) { // if fields are not empty...
 
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
-      const fileExt = fileName.split('.').slice(-1)[0]; 
+      const fileExt = fileName.split('.').slice(-1)[0];
 
-    //   if(title.length <= 25 && author.length <= 50 && ){
-
-    //     if(fileExt == 'gif' || fileExt == 'jpg' || fileExt == 'png'){
 
       if(title.length <= 25 && author.length <= 50 && (fileExt == 'gif' || fileExt == 'jpg' || fileExt == 'png')){
 
-        let email = RegExp.prototype.test.bind(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+        let emailTest = RegExp.prototype.test.bind(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
         let html = RegExp.prototype.test.bind(/[^a-zA-Z]*$/);
 
 
-        if(html(title) === false && email(email) === true){
+        if(html(title) === false && emailTest(email) === true){
 
           const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
 
@@ -59,41 +56,36 @@ exports.loadAll = async (req, res) => {
 exports.vote = async (req, res) => {
 
   try {
-    const photoToUpdate = await Photo.findOne({ _id: req.params.id });
-    const existingVote = await Vote.findOne({ user: userIp });
     const userIp = requestIp.getClientIp(req);
-
+    const photoToUpdate = await Photo.findOne({ _id: req.params.id });
+    const existingVote = await Voter.findOne({ user: userIp });
 
     if(!photoToUpdate) res.status(404).json({ message: 'Not found' });
-
 
     // record for a given IP adress doeasn't exist...
     else if(!existingVote){
 
-      newVote = new Vote({ user: userIp, votes: req.params.id });
+      const newVote = new Voter({ user: userIp, votes: [req.params.id] });
       await newVote.save();
-      res.json(newVote);
 
       photoToUpdate.votes++;
-      photoToUpdate.save();
-      
-    }
+      await photoToUpdate.save();
 
+      res.json(newVote);
+    }
     // record for a given IP adress exist...
     else {
-      if(existingVote.votes.includes(photoToUpdate._id)){    // CL ??
-        res.send('Only one voice per photo !');
-
-      } else { 
-
+      if(existingVote.votes.includes(photoToUpdate._id)) {
+        res.status(500).send('Only one voice per photo !');
+      } else {
         existingVote.votes.push(photoToUpdate._id)
-        existingVote.save();
+        await existingVote.save();
 
         photoToUpdate.votes++;
-        photoToUpdate.save();
+        await photoToUpdate.save();
         res.send({ message: 'OK' });
       }
-      
+
     }
   } catch(err) {
     res.status(500).json(err);
